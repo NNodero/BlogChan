6f3548ffd8c371a0466e501ecbbdb60a4e00a98d
Nischal Shrestha <nischalstha14@gmail.com>
2023-08-09 18:21:02 +1000

    asd

--- server/JWT.js
+++
@@ -1,8 +0,0 @@
-const {sign} =require('jsonwebtoken')
-
-const createTokens=(data)=>{
-    const accesstoken = sign({id: data.id, username:data.username,phone:data.phone}, 'thisismyfirsttime',{expiresIn:'1d'});
-    return accesstoken
-}
-
-module.exports = {createTokens}
