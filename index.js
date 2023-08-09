1c72b35de67f06a9263f3ef2c07347d85da763b0
Nischal Shrestha <nischalstha14@gmail.com>
2023-08-09 18:33:18 +1000

    main

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT,()=>{
