8d891c9f74028ad9734c52b326a3532cf3365caa
Nischal Shrestha <nischalstha14@gmail.com>
2023-08-28 18:09:22 +1000

    Update Profile.jsx

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,9 +1,16 @@
-import './Profile.scss'
+import p from'./Profile.scss'
 
 import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <div className={p.maincontainer}>
+        <div className={p.left}></div>
+          
+        <div className={p.right}></div>
+
+      </div>
+    </div>
   )
 }
