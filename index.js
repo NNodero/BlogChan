f93dd7800ac922840676188a31b7b79df4dcab85
Nischal Shrestha <nischalstha14@gmail.com>
2023-10-23 23:20:07 +1100

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express());
 // app.use(cors());
 
 app.use(cors({
-    origin:["https://blogchan.onrender.com", "http://localhost:3000"],
+    origin:["https://blogchan.onrender.com", "http://localhost:3000","http://192.168.1.3:3000"],
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
     credentials: true,
 
